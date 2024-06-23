ffafa0f74b2275a6ff7b2357afbe28ca225f66e9
vm622 <vm62285@gmail.com>
2024-06-24 00:59:05 +0300

    minor fix

--- product-service/lambda_functions/products.py
+++ product-service/lambda_functions/products.py
@@ -1,5 +1,5 @@
 products_list = [
-    {"id": "1", "name": "Product 1 description", "price": 142},
-    {"id": "2", "name": "Product 2 description", "price": 256.34},
-    {"id": "3", "name": "Product 3 description", "price": 367},
+    {"id": "1", "title": "Product 1", "description": "Product 1 description", "price": 142},
+    {"id": "2", "title": "Product 2", "description": "Product 2 description", "price": 256.34},
+    {"id": "3", "title": "Product 3", "description": "Product 3 description", "price": 367},
 ]
