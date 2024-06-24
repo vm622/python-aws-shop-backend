c77ae4b3fd371bb46fbed78d424b08d4e77ccad8
vm622 <vm62285@gmail.com>
2024-06-24 14:56:02 +0300

    remove products mock

--- product-service/lambda_functions/products.py
+++
@@ -1,5 +0,0 @@
-products_list = [
-    {"id": "1", "title": "Product 1", "description": "Product 1 description", "price": 142},
-    {"id": "2", "title": "Product 2", "description": "Product 2 description", "price": 256.34},
-    {"id": "3", "title": "Product 3", "description": "Product 3 description", "price": 367},
-]
