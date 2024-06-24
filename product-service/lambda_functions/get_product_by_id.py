03487d705ddc976bc879685ce253455668144194
vm622 <vm62285@gmail.com>
2024-06-24 15:01:21 +0300

    connect products lambdas to dynamodb tables

--- product-service/lambda_functions/get_product_by_id.py
+++ product-service/lambda_functions/get_product_by_id.py
@@ -1,42 +1,61 @@
 import json
-from products import products_list
+import os
+import boto3
 
 def handler(event, context):
-    product_id = event["pathParameters"]["id"]
+    try:
+        product_id = event["pathParameters"]["id"]
 
-    if not product_id:
+        dynamodb = boto3.resource('dynamodb', region_name=os.getenv('AWS_REGION'))
+
+        products_table_name = os.getenv('PRODUCTS_TABLE_NAME')
+        stocks_table_name = os.getenv('STOCKS_TABLE_NAME')
+
+        products_table = dynamodb.Table(products_table_name)
+        stocks_table = dynamodb.Table(stocks_table_name)
+
+        product_item = products_table.get_item(Key={'id': product_id})
+        stock_item = stocks_table.get_item(Key={'product_id': product_id})
+
+        if 'Item' in product_item and 'Item' in stock_item:
+            product = {
+                'id': product_item['Item']['id'],
+                'title': product_item['Item']['title'],
+                'description': product_item['Item']['description'],
+                'price': str(product_item['Item']['price']),
+                'count': str(stock_item['Item']['count']),
+            }
+
+            return {
+                "statusCode": 200,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps(product)
+            }
+        else:
+            return {
+                "statusCode": 404,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps(f"'message': 'Product {product_id} not found'")
+            }
+
+    except Exception as e:
         return {
-            "statusCode": 404,
+            "statusCode": 500,
             "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "GET",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Content-Type": "application/json"
             },
-            "body": json.dumps({"message": "Product ID is required"})
-        }
-    
-    product = next((p for p in products_list if p["id"] == product_id), None)
-
-    if not product:
-        return {
-            "statusCode": 404,
-            "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Content-Type": "application/json"
-            },
-            "body": json.dumps({"message": "Product not found"})
-        }
-
-    return {
-        "statusCode": 200,
-        "headers": {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Content-Type": "application/json"
-            },
-        "body": json.dumps(product)
-    }
+            "body": json.dumps({"message": str(e)})
+        } 
--- product-service/lambda_functions/get_products_list.py
+++ product-service/lambda_functions/get_products_list.py
@@ -1,26 +1,59 @@
 import json
-from products import products_list
+import os 
+import boto3
 
 def handler(event, context):
-    if not products_list:
+    try:
+        dynamodb = boto3.resource('dynamodb', region_name=os.getenv('REGION'))
+        products_table = dynamodb.Table(os.getenv('PRODUCTS_TABLE_NAME'))
+        stocks_table = dynamodb.Table(os.getenv('STOCKS_TABLE_NAME'))
+
+        products_response = products_table.scan()
+        products_items = products_response.get('Items', [])
+
+        stocks_response = stocks_table.scan()
+        stocks_items = stocks_response.get('Items', [])
+
+        if not products_items:
+            return {
+                "statusCode": 404,
+                "headers": {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Content-Type": "application/json"
+                },
+                "body": json.dumps({"message": "No products found"})
+            }
+
+        products_dict = {item['id']: item for item in products_items}
+
+        for item in stocks_items:
+            product_id = item['product_id']
+            if product_id in products_dict:
+                products_dict[product_id]['count'] = int(item['count'])
+                products_dict[product_id]['price'] = int(products_dict[product_id]['price'])
+
+        products = list(products_dict.values())
         return {
-            "statusCode": 404,
+        "statusCode": 200,
             "headers": {
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "GET",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Content-Type": "application/json"
             },
-            "body": json.dumps({"message": "No products found"})
+            "body": json.dumps(products)
         }
-    
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET",
-            "Access-Control-Allow-Headers": "Content-Type",
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps(products_list)
-    }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"message": str(e)})
+        }
